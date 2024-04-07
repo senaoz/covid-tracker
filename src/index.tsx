8077692263617e80245b19d564e7d8c596a76630
Sena Oz <tazeyta@gmail.com>
2024-04-07 16:27:20 +0300

    add Router structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./pages/App";
-import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import CountryPage from "./pages/CountryPage";
+import { fetchReports } from "./services/fetchReports";
+import MapPage from "./pages/MapPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MapPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "country/:isoCode/:date?",
+    element: <CountryPage />,
+    loader: async ({ request, params }) => {
+      return fetchReports(params.isoCode, params.date);
+    },
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
-root.render(<App />);
 
-reportWebVitals();
+root.render(
+  <>
+    <RouterProvider router={router} />
+  </>,
+);
